a2ec45244508290d1e0b533fdcc0e2651c0fe212
Shawn <sgraven66@gmail.com>
2020-11-20 08:00:38 -0600

    Moved away from this due to issues #7

--- Brute-Force-Decoder/queue.c
+++ Brute-Force-Decoder/queue.c
@@ -1,39 +1,34 @@
-this#include "queue.h"
+#include "queue.h"
 
 struct node* DUMMY={NULL, NULL};
 
-void push(void* data){
-  struct node* n=malloc(sizeof(struct node));
-  n->data=data;
-  n->next=NULL;
-  pthread_mutex_lock(&this->endLock);
-  this->end->next=n;
-  this->end=n;
-  pthread_mutex_lock(&this->endLock);
+void queue_push(struct queue* this, void* data){
+	struct node* n=malloc(sizeof(struct node));
+	n->data=data;
+	n->next=NULL;
+	pthread_mutex_lock(&(this->endLock));
+	this->end->next=n;
+	this->end=n;
+	pthread_mutex_lock(&(this->endLock));
 }
 
-void* pop(){
+void* queue_pop(struct queue* this){
 	assert(this!=NULL);
-  if(this->front==DUMMY)return NULL;
-  pthread_mutex_lock(&this->frontLock);
-  struct node* start=this->front;
-  this->front=start->next;
-  pthread_mutex_unlock(&this->frontLock);
-  void* data=start->data;
-  free(start);
-  return data;
+	if(this->front==DUMMY)return NULL;
+	pthread_mutex_lock(&(this->frontLock));
+	struct node* start=this->front;
+	this->front=start->next;
+	pthread_mutex_unlock(&(this->frontLock));
+	void* data=start->data;
+	free(start);
+	return data;
 }
 
-static struct queue* _this(struct queue* that){
-	return this = that;
-}
-
-void* queue_init(){
+struct queue* queue_init(){
 	struct queue* q=malloc(sizeof(struct queue*));
 	q->front=DUMMY;
 	q->end=DUMMY;
 	q->frontLock=PTHREAD_MUTEX_INITIALIZER;
 	q->endLock=PTHREAD_MUTEX_INITIALIZER;
-	q->_this(queue);
 	return q;
 }
--- Brute-Force-Decoder/queue.h
+++ Brute-Force-Decoder/queue.h
@@ -1,23 +1,24 @@
 #ifndef LOCKED_QUEUE_H
 #define LOCKED_QUEUE_H
 #include <pthread.h>
+#include <unistd.h> 
+#include <stdlib.h> 
 
 struct node{
-  struct node* next;
-  void* data;
+	struct node* next;
+	void* data;
 };
 
-static struct Stack* this;
-static struct queue* _this(struct queue* that);
-
 struct queue{
-	struct queue (*_this)(struct Stack *);
 	struct node* front;
 	pthread_mutex_t frontLock;
 	struct node* end;
 	pthread_mutex_t endLock;
-	void (*push)(void* data);
-	void* (*pop)();
-}
+};
+
+void queue_push(struct queue* this, void* data);
+void* queue_pop(struct queue* this);
+struct queue* queue_init();
+
 
 #endif
