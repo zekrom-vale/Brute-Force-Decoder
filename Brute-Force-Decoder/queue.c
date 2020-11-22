9ccfddca9e2e8faa4703fc6026d7536a4fa97013
Shawn <sgraven66@gmail.com>
2020-11-21 19:35:42 -0600

    Removed branching complexity #7
    The other way should work, but still.

--- Brute-Force-Decoder/queue.c
+++ Brute-Force-Decoder/queue.c
@@ -122,15 +122,15 @@ void* queue_pop(struct queue* this){
 	this->front->next=start->next;
 
 	//If the node to remove is at the end
-	if(start->next==NULL){
-		queue_unlockFront(this);
+	bool end=start->next==NULL;
+	queue_unlockFront(this);
+	if(end){
 		//Lock the other side
 		queue_lockEnd(this);
 		//Update the end
 		this->end=this->DUMMY;
 		queue_unlockEnd(this);
 	}
-	else queue_unlockFront(this);
 	//Extract the data
 	void* data=start->data;
 	//Free the node
@@ -191,23 +191,23 @@ void queue_forEach(struct queue* this, bool (*f)(int i, void* data)){
 	int i=0;
 	queue_lockFront(this);
 	struct node* cur=this->front->next;
+	bool flag=false;
 	if(cur!=NULL){
 		while(cur->next!=NULL){
 			if(!f(i++, cur->data)){
-				queue_unlockFront(this);
-				return;
+				flag=true;
+				break;
 			}
 			cur=cur->next;
 		}
-		//Done with the front
-		queue_unlockFront(this);
-
-		//Lock the end
-		queue_lockEnd(this);
-		f(i, cur->data);
-		queue_unlockEnd(this);
 	}
-	else queue_unlockFront(this);
+	queue_unlockFront(this);
+	if(flag)return;
+
+	//Lock the end
+	queue_lockEnd(this);
+	f(i, cur->data);
+	queue_unlockEnd(this);
 }
 
 /** 
